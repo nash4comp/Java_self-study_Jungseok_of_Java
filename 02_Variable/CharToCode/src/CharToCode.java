6fdcec9d32cb2dc2fe446a9e19ff3aa035c43671
Nash Baek <nash4comp@gmail.com>
2021-11-18 15:32:36 -0800

    Chapter 02

---
+++ 02_Variable/CharToCode/src/CharToCode.java
@@ -0,0 +1,16 @@
+
+class CharToCode 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		char ch = 'A';
+		int code = (int)ch;
+		
+		System.out.printf("%c=%d(%#X)%n", ch, code, code);
+		char hch = '°¡';
+		System.out.printf("%c=%d(%#X)%n", hch, (int)hch, (int)hch);
+	}
+
+}
